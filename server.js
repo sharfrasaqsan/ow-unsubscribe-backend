505e62db4be820378a03e1b697777e24537a4cc7
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2023-10-11 15:54:34 +0530

    server updates

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios"); // Import Axios
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const SECRET_KEY = "6LepYnkoAAAAANdKmTb899omcgUo3avxIAVzn_Te";
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const cors = require("cors");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-module.exports = app;
+app.post("/verify", (req, res) => {
+  const VERIFY_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${req.body["g-recaptcha-response"]}`;
+
+  axios
+    .post(VERIFY_URL)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: "An error occurred" });
+    });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running successfuully simple !" });
+});
+
+app.listen(port, () => {
+  console.log("Server started on: " + port);
+});
