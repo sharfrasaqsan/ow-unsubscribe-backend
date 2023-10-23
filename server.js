7899fb6d3b7c9dd140a26e80f03d23065cd19417
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2023-10-23 12:28:26 +0530

    Submission stored JSON

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require("axios"); // Import Axios
+const axios = require("axios");
+const winston = require("winston");
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,22 +13,43 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: "submission-logs.log" }),
+  ],
+});
+
 app.post("/verify", (req, res) => {
   const VERIFY_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${req.body["g-recaptcha-response"]}`;
 
   axios
     .post(VERIFY_URL)
     .then((response) => {
+      // Capture the current timestamp
+      const submissionTimestamp = new Date();
+
+      // Log the submission data along with the timestamp
+      logger.info({
+        timestamp: submissionTimestamp,
+        email: req.body.email,
+        selectedTypes: req.body.selectedTypes,
+        selectedReasons: req.body.selectedReasons,
+        verificationStatus: response.data.success ? "Success" : "Failure",
+      });
+
       res.send(response.data);
     })
     .catch((error) => {
+      logger.error("Error occurred during verification:", error);
       console.error(error);
       res.status(500).json({ error: "An error occurred" });
     });
 });
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Server is running successfuully simple !" });
+  res.status(200).json({ message: "Server is running successfully!" });
 });
 
 app.listen(port, () => {
